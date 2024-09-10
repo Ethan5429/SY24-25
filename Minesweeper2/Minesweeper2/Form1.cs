145c29e50be340e6264e094342f80f5d0b5ecbf7
Ethan <erdai979@learner.dvusd.org>
2024-09-10 09:15:07 -0700

    Minesweeper added

---
+++ Minesweeper2/Minesweeper2/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minesweeper2
+{
+    public partial class Form1 : Form
+    {
+        Button[] btnGrid = new Button[100];
+        Tile[] tileGrid = new Tile[100];
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 100; i++)
+            {
+                btnGrid[i] = (Button)Controls["button" + (i + 1)];
+                tileGrid[i] = new Tile(btnGrid[i]);
+                btnGrid[i].BackColor = Color.Green;
+            }
+        }
+
+        private Button GetButton(int r, int c)
+        { 
+        return (Button)GetButton(r, c);
+        }
+
+        private int getIndex(Button b)
+        { 
+            string tmp = b.Name.Substring(6);
+            int retVal = 0;
+            int.TryParse(tmp, out retVal);
+            return retVal-1;
+        }
+
+
+        private void button100_MouseDown(object sender, MouseEventArgs e)
+        {
+            Button b = sender as Button;
+            b.BackColor = Color.Red;
+        }
+    }
+}
