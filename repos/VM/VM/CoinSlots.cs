a00916c02ac664417648d832b441a6aba8fdd0c4
Ethan <erdai979@learner.dvusd.org>
2025-02-07 08:25:50 -0700

    vm added

---
+++ repos/VM/VM/CoinSlots.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VM
+{
+    internal class CoinSlots
+    {
+        private double m_Total;
+        public void insert(string coin)
+        {
+            if (coin == "quarter"){m_Total += .25;}
+            if (coin == "dime") { m_Total += .10; }
+            if (coin == "nickel") { m_Total += .05; }
+            if (coin == "dollar") { m_Total += 1; }
+        }
+
+        public void coinReturn()
+        {
+            m_Total = 0;
+        }
+
+        public double total()
+        {
+            return m_Total;
+        }
+
+    }
+}
