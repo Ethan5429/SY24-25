814166898a4624f8bb95ee4cedf85f9f1f1ec59a
Ethan <erdai979@learner.dvusd.org>
2025-02-14 09:30:35 -0700

    2/14/25

--- quartet/quartet/CarCard.cs
+++ quartet/quartet/CarCard.cs
@@ -8,11 +8,77 @@ namespace quartet
 {
     internal class CarCard
     {
-       public int maxspeed { get; set; }
-        public double zeroTO60 { get; set; }
-        public int HP { get; set; }
+        public enum category { maxspeed, CC, zeroTo60, cylinders, HP, RPM, ID}
+        public string name { get; set; }
+        public int maxspeed { get; set; }
         public int CC { get; set; }
+        public double zeroTo60 { get; set; }
         public int cylinders { get; set; }
+        public int HP { get; set; }
         public int RPM { get; set; }
+        public int ID { get; set; }
+
+        public CarCard(string name, int maxspeed, int CC, double zeroTo60, int cylinders, int HP, int RPM, int ID)
+        {
+            this.name = name;
+            this.maxspeed = maxspeed;
+            this.CC = CC;
+            this.zeroTo60 = zeroTo60;
+            this.cylinders = cylinders;
+            this.HP = HP;
+            this.RPM = RPM;
+            this.ID = ID;
+        }
+        public CarCard compare(CarCard other, category c)
+        {
+            if (other == null) return this;
+            if (c == category.maxspeed)
+                if (maxspeed > other.maxspeed)
+                    return this;
+                else
+                    return other;
+
+            if (c == category.CC)
+                if (CC > other.CC)
+                    return this;
+                else
+                    return other;
+
+            if (c == category.zeroTo60)
+                if (zeroTo60 > other.zeroTo60)
+                    return this;
+                else
+                    return other;
+
+            if (c == category.cylinders)
+                if (cylinders > other.cylinders)
+                    return this;
+                else
+                return other;
+
+            if (c == category.HP)
+                if (HP > other.HP)
+                    return this;
+                else
+                return other;
+
+            if (c == category.RPM)
+                if (RPM > other.RPM)
+                    return this;
+                else
+                return other;
+
+            return null;
+        }
+
+        public override string ToString()
+        {
+            return name + ":\n\tmaxspeed:" + maxspeed 
+                + "\n\tzeroTo60:" + zeroTo60
+                + "\n\tcylinders:" + cylinders
+                + "\n\tHP:" + HP
+                + "\n\tRPM:" + RPM
+                + "\n\tID:" + ID;
+        }
     }
 }
--- quartet/quartet/Program.cs
+++ quartet/quartet/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace quartet
 {
@@ -10,6 +11,13 @@ namespace quartet
     {
         static void Main(string[] args)
         {
+            CarCard C4 = new CarCard("Corvette Coupe", 281, 5665, 5.2, 8, 344, 5400, 3);
+            CarCard C3 = new CarCard("Porsche 911 Targa", 285, 3596, 5.2, 6, 320, 6800, 3);
+            CarCard F4 = new CarCard("Fisker Tramonto", 325, 5439, 3.6, 8, 610, 6100, 6);
+            CarCard E3 = new CarCard("BMW 645 Ci", 250, 4398, 5.6, 8, 333, 6100, 5);
+            CarCard D1 = new CarCard("Audi RS4", 250, 4163, 4.8, 8, 420, 7800, 4);
+            WriteLine(C4.compare(C3, CarCard.category.cylinders));
+            ReadLine();
         }
     }
 }
